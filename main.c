e07239ed3640d1861a12be45e92706dfae35c8c3
xim1k <mr.mikhailoiv2013@mail.ru>
2018-05-22 22:56:45 +0700

    Add functions: move, check_sim

--- main.c
+++ main.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+int check_sim(char A) {
+  switch (A){
+    case 'a':
+      return 0;
+    case 'b':
+      return 1;
+    case 'c':
+      return 2;
+    case 'd':
+      return 3;
+    case 'e':
+      return 4;
+    case 'f':
+      return 5;
+    case 'g':
+      return 6;
+    case 'h':
+      return 7;
+  }
+}
 
 
 void print_board(char board[8][8]) {
@@ -19,9 +39,26 @@ void print_board(char board[8][8]) {
 
 }
 
+void move(char board[8][8], int c){
+  char first[6];
+  char tim;
+  int i , j;
+  printf("Input position of figure:");
+  scanf("%s", first);
+  j = check_sim(first[0]);
+  i = first[1] - '0' + 1;
+  tim = board[i][j];
+  board[i][j] = ' ';
+  i = check_sim(first[3]);
+  j = first[4] - '0' + 1;
+  board[i][j] = tim;
+
+}
+
 
 int main() {
     int i, j;
+    int k = 0;
     char board[][8] = {
       "rnbqkbnr",
       "pppppppp",
@@ -33,5 +70,14 @@ int main() {
       "RNBQKBNR"
     };
     print_board(board);
+    while(1) {
+      move(board, k);
+      print_board(board);
+      if(k = 0){
+        k = 1;
+      } else {
+        k = 0;
+      }
+    }
     return 0;
 }
